42171904dfaaa4262c58cfbded26b87a0a4e1bc8
Eshwar0601 <852eshwar@gmail.com>
2021-07-28 18:16:48 +0530

    Actions and Reducers

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,23 @@
+import { SET_CURRENT_USER } from "../actions/types";
+import { isEmpty } from "lodash";
+import { isElement } from "react-dom/cjs/react-dom-test-utils.production.min";
+
+const initialState = {
+    isAuthenticated = false,
+    user : {
+
+    }
+}
+
+export default function(state= initialState, action) {
+    switch(action.type) {
+        case SET_CURRENT_USER:
+            return {
+                ...state, 
+                isAuthenticated: !isEmpty(action.payload),
+                user : action.payload
+            }
+        default: 
+            return state
+    }
+}
